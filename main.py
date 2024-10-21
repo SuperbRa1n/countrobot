18ea80b409446ac7fa3a0f00e5d0f3065a4501c9
SuperbRa1n <tanninrachel@yinlin.wiki>
2024-10-21 16:17:57 +0800

    change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -143,4 +144,4 @@ def count(person):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host="0.0.0.0")
